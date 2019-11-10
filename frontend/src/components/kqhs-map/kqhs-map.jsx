cf4fb8353b2d342ffd509af040e572bc81d951d6
nate2427 <nlbaker@umich.edu>
2019-11-10 07:33:16 -0600

    creating the outline of the grand rapids with lat/long coordinates

--- frontend/src/components/kqhs-map/kqhs-map.jsx
+++ frontend/src/components/kqhs-map/kqhs-map.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps"
+import { GoogleMap, Marker, withGoogleMap, withScriptjs, Polygon } from "react-google-maps"
+
 
 
 import './kqhs-map.css';
+
+
+const coords = [
+    { lat: 43.007345, lng: -85.741812 },
+    { lat: 43.007752, lng: -85.728409 },
+    { lat: 42.999067, lng: -85.672861 },
+    { lat: 43.029051, lng: -85.660880 },
+    { lat: 43.028673, lng: -85.636183 },
+    { lat: 43.020460, lng: -85.622967 },
+    { lat: 43.009548, lng: -85.620131 },
+    { lat: 42.992041, lng: -85.610481 },
+    { lat: 43.003854, lng: -85.594709 },
+    { lat: 43.003083, lng: -85.586541 },
+    { lat: 42.977408, lng: -85.590790 },
+    { lat: 42.977191, lng: -85.580241 },
+    { lat: 42.970063, lng: -85.580350 },
+    { lat: 42.969879, lng: -85.590757 },
+    { lat: 42.962467, lng: -85.590937 },
+    { lat: 42.957746, lng: -85.630053 },
+    { lat: 42.941006, lng: -85.629704 },
+    { lat: 42.941122, lng: -85.580476 },
+    { lat: 42.927076, lng: -85.578615 },
+    { lat: 42.927017, lng: -85.570010 },
+    { lat: 42.913010, lng: -85.569111 },
+    { lat: 42.883718, lng: -85.606674 },
+    { lat: 42.884764, lng: -85.645615 },
+    { lat: 42.921333, lng: -85.687056 },
+    { lat: 42.938174, lng: -85.725333 },
+    { lat: 42.965399, lng: -85.741674 },    
+    { lat: 43.008199, lng: -85.742805 },    
+    { lat: 43.007949, lng: -85.730388 },
+    { lat: 43.000205, lng: -85.672863 },
+];
+
+const reversedCoords = coords.map( ll => {
+    return { lat: ll.lng, lng: ll.lat }
+});
 
 
 export default class KQHSMap extends React.Component {
 
 
     render = () => {
+        console.log(process.env.REACT_APP_GOOGLE_KEY);
         
         return(
             <React.Fragment>
@@ -32,9 +71,10 @@ export default class KQHSMap extends React.Component {
                     </div>
                     <div className="embbed kqhs-google-map">
                         <div style={{ height: '100%', width: '100%', position: 'relative'}}>
-                            <MyMapComponent i
-                                sMarkerShown 
-                                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                            <KQHSGoogleMap 
+                                isMarkerShown 
+                                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places
+                                                &key=`}
                                 loadingElement={<div style={{ height: `100%` }} />}
                                 containerElement={<div style={{ height: `400px` }} />}
                                 mapElement={<div style={{ height: `100%` }} />}
@@ -48,11 +88,19 @@ export default class KQHSMap extends React.Component {
 }
 
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const KQHSGoogleMap = withScriptjs(withGoogleMap((props) =>
     <GoogleMap
-        defaultZoom={8}
-        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        defaultZoom={9}
+        defaultCenter={{ lat: 42.863360, lng: -85 }}
     >
-        {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+        <Polygon
+            path={reversedCoords}
+            options={{
+                fillColor: "#8A191B",
+                fillOpacity: 0.4,
+                strokeColor: "#8A191B",
+                strokeOpacity: 1,
+                strokeWeight: 1
+        }} />
     </GoogleMap>
 ));
