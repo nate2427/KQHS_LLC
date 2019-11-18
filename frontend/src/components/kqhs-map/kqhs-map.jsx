8ed6561ff07bf7c0e639e43dfcaf1ea1efedec6f
nate2427 <nlbaker@umich.edu>
2019-11-18 15:08:10 -0600

    hard coded the google key

--- frontend/src/components/kqhs-map/kqhs-map.jsx
+++ frontend/src/components/kqhs-map/kqhs-map.jsx
@@ -71,7 +71,7 @@ export default class KQHSMap extends React.Component {
                             <KQHSGoogleMap 
                                 isMarkerShown 
                                 googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places
-                                                &key=${process.env.REACT_APP_GOOGLE_KEY}`}
+                                                &key=${'AIzaSyC9HXvW8F0PhJAumPLVCTuXCOiPGlYPW38'}`}
                                 loadingElement={<div style={{ height: `100%` }} />}
                                 containerElement={<div style={{ height: `400px` }} />}
                                 mapElement={<div style={{ height: `100%` }} />}
